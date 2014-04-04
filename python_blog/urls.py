2fca3cb3893ec80fdb92543fa8fb7dfe75bf8dcb
Nimdis <tsaazi@gmail.com>
2014-04-04 19:03:02 +0700

    urls and views functions

--- python_blog/urls.py
+++ python_blog/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'python_blog.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'blog.views.index'),
+    url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
 )
